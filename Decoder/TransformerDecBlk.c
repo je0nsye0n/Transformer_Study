3cfa94a14d883d9234928c7f680b50bc7c9d4cce
jeon-syeon <tjjgus909052@gmail.com>
2025-02-17 16:16:13 +0900

    data file upload

---
+++ Decoder/TransformerDecBlk.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void DecoderBlock(float ***input, float ***output, int batch, int seq_length, int dim_model, int ffn_hidden, int n_head){
+
+}
---
+++ Decoder/TransformerDecBlk.h
@@ -0,0 +1,1 @@
+void DecoderBlock(float ***input, float ***output, int batch, int seq_length, int dim_model, int ffn_hidden, int n_head);
